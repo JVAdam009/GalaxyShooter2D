e0dd76cebb000265878bdbea19bc8b71bdb7b1d9
JVAdam009 <Harlock@duck.com>
2022-07-01 13:47:11 -0400

    Laser created

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -16,8 +16,35 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+    CaluclateMovement();
+    }
+
+    void CaluclateMovement()
+    {
         float horizontalInput = Input.GetAxis("Horizontal");
+      
         float verticalInput = Input.GetAxis("Vertical");
+       
         transform.Translate(((Vector3.right * horizontalInput) + (Vector3.up * verticalInput)) * _speed * Time.deltaTime);
+        
+        
+        if (transform.position.y > 0)
+        {
+            transform.position = new Vector3(transform.position.x, 0, 0);
+        }
+        else if (transform.position.y <= -3.8f)
+        {
+            transform.position = new Vector3(transform.position.x, -3.8f, 0);
+        }
+        
+        
+        if (transform.position.x > 13.4f)
+        {
+            transform.position = new Vector3(-13.4f,transform.position.y,  0);
+        }
+        else if (transform.position.x <= -13.4f)
+        {
+            transform.position = new Vector3(13.4f,transform.position.y,  0);
+        }
     }
 }
