69c0598ace9f7bd70e4b761520beac5d9a3d9ac7
JVAdam009 <Harlock@duck.com>
2022-07-01 18:26:14 -0400

    Shooting ability complete

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,8 +5,14 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float _speed = 3.5f;
+
+    [SerializeField] private GameObject LaserObjPrefab;
     
-     
+    [SerializeField] private float _laserVerticalOffset = 0.7f;
+
+    [SerializeField] private float _fireCoolDown = -1f;
+
+    [SerializeField] private float _fireRate = 0.5f;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +23,17 @@ public class Player : MonoBehaviour
     void Update()
     {
     CaluclateMovement();
+    
+    FireLaser();
+    }
+
+    void FireLaser()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > _fireCoolDown)
+        {
+            Instantiate(LaserObjPrefab, transform.position+new Vector3(0,_laserVerticalOffset,0), Quaternion.identity);
+            _fireCoolDown = Time.time + _fireRate;
+        }
     }
 
     void CaluclateMovement()
